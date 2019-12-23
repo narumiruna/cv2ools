b0d249e5651e4dfeb794fee4f602f32c305ac013
なるみ <weaper@gmail.com>
2019-12-23 11:45:59 +0800

    Update merge_images_to_video.py

--- merge_images_to_video.py
+++ merge_images_to_video.py
@@ -2,9 +2,6 @@ import argparse
 import os
 from glob import glob
 
-import cv2
-from tqdm import tqdm
-
 import cv2ools
 
 
@@ -16,19 +13,11 @@ def parse_args():
     return parser.parse_args()
 
 
-def read_images(paths):
-    for path in tqdm(paths):
-        img = cv2.imread(path)
-        if img is not None:
-            yield img
-        else:
-            break
-
-
 def main():
     args = parse_args()
     paths = sorted(glob(os.path.join(args.root, '*.jpg')))
-    cv2ools.write_video(read_images(paths), args.output)
+    images = cv2ools.read_images(paths)
+    cv2ools.write_video(images, args.output)
 
 
 if __name__ == "__main__":
