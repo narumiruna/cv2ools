72a29cb1217c2d026c0a85e08891068f7a91da01
なるみ <weaper@gmail.com>
2019-12-26 10:15:26 +0800

    Move VideoStream

--- cv2ools/__init__.py
+++ cv2ools/__init__.py
@@ -1,5 +1,5 @@
+from .core import VideoStream
 from .core import VideoWriter
 from .functional import read_images
 from .functional import read_video
 from .functional import write_video
-from .stream import VideoStream
