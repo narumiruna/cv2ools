074888500db67d19ae516c9fae8975752bd77ae2
なるみ <weaper@gmail.com>
2020-02-15 15:41:39 +0800

    Add setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,12 @@ from setuptools import setup
 
 
 def parse_requirements(f):
-    lines = []
     with open(f, 'r') as fp:
         for line in fp.readlines():
-            lines.append(line.strip())
-    return lines
+            yield line.strip()
 
 
 def main():
-    requirements = parse_requirements('requirements.txt')
-
     setup(
         name='cv2ools',
         use_scm_version=True,
@@ -20,7 +16,7 @@ def main():
         author='なるみ',
         author_email='weaper@gamil.com',
         packages=find_packages(),
-        install_requires=requirements,
+        install_requires=list(parse_requirements('requirements.txt')),
     )
 
 
